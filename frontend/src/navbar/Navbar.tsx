98c0c49b20d9c7f99e7e7e85a7fe84d0585285ec
LudwigK0ch <lucky.koch@gmx.de>
2025-02-05 14:28:08 +0100

    Added navbar component.

---
+++ frontend/src/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <a className="active">Campaigns</a>
+            <a>Players</a>
+            <a>Encounter</a>
+            <a>Items & Rules</a>
+        </div>
+    );
+};
+
+export default Navbar;
