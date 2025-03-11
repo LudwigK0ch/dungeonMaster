14e813cd9032a22002bc200d32043862b1d684a5
LudwigK0ch <lucky.koch@gmx.de>
2025-03-11 11:17:12 +0100

    Included bootstrap and used bootstrap navbar.

--- frontend/src/navbar/Navbar.tsx
+++ frontend/src/navbar/Navbar.tsx
@@ -1,15 +1,36 @@
 
-import "./Navbar.css"
-
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <a className="active">Campaigns</a>
-            <a>Players</a>
-            <a>Encounter</a>
-            <a>Items & Rules</a>
-        </div>
-    );
+	return (
+		<nav className="navbar navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+			<a className="navbar-brand" href="#">
+				DungeonMaster
+			</a>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNavAltMarkup"
+				aria-controls="navbarNavAltMarkup"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div className="navbar-nav">
+					<a className="nav-item nav-link" href="#">
+						Home
+					</a>
+					<a className="nav-item nav-link" href="#">
+						Campaigns
+					</a>
+					<a className="nav-item nav-link" href="#">
+						Encounter
+					</a>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
