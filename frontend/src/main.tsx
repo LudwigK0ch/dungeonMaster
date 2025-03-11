45ef630868553516c720f878c3bd7f65790edae3
LudwigK0ch <lucky.koch@gmx.de>
2025-03-11 12:06:09 +0100

    Updated main.tsx to new components folder

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import Navbar from './navbar/Navbar'
+import Navbar from './components/navbar/Navbar'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
